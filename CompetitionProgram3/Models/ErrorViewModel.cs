24baf714d1e4cc570a35a4e685841b8940adfbb6
Emily Nicholson <ernicholson22@gmail.com>
2018-11-08 08:42:43 -0500

    Add project files.

---
+++ CompetitionProgram3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompetitionProgram3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
